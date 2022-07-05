ed21adb5170262c88e6450970d4c64d799d22a92
yentl <yentlcast@gmail.com>
2022-07-04 22:26:11 -0300

    start button functionality & conditional rendering of quiz page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Question from './Question';
+
 
 function App() {
+
+  //HOMEPAGE
+  let homepage= <section>
+    <h1>Quizzical</h1>
+    <p>description</p>
+    <button onClick={()=>startBtn()}>Start</button>
+  </section>
+  
+  //VALUE THAT CHANGES THE CONDITIONAL RENDERING OF HOMEPAGE
+  const [start, setStart]= useState(false)
+  
+  // START BTN 
+  function startBtn(){
+      setStart(true)
+      console.log("started")
+    }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {start? < Question/> :homepage}
     </div>
   );
 }
---
+++ src/Question.js
@@ -0,0 +1,8 @@
+export default function Question(){
+    return(
+        <section>
+            <h2>question</h2>
+            <p>answer options</p>
+        </section>
+    )
+}
