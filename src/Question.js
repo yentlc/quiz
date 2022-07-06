3c804feccb4a4a8486a8deb76119b787343d8f62
yentl <yentlcast@gmail.com>
2022-07-06 15:56:51 -0300

    I can't see the options

--- src/Question.js
+++ src/Question.js
@@ -1,29 +1,30 @@
+
+
 export default function Question(props){
   //SAVE THE ANSWERS ARRAY
-    const answersArr = props.answers
+    const answersArr= props.answers
 
     // SHUFFLE THE ARRAY FUNCTION
     function shuffleArr(arr){
         //create a new array to save the shuffled array
-        let prevArr= arr
+        const prevArr= arr
         const shuffled=[]
         //Loop through the array selecting a random index, saving it in the new array and removing it from the prior
-        while(prevArr.length>0){
+        while(arr.length>0){
             const randomIndex= Math.floor(Math.random()*prevArr.length)
             shuffled.push(prevArr[randomIndex])
             prevArr.splice(randomIndex,1)
         }
-        
         return shuffled
     }
-
-
-    console.log(answersArr, shuffleArr(answersArr))
+    
+    let options= shuffleArr(answersArr)
+    console.log(options)
     return(
         <fieldset>
             <legend>{props.question}</legend>
-            <input type="radio" name={props.question} value={1} id={1} />
-            <p>answer options</p>
+
+            <p>{options[1]}option2</p> 
         </fieldset>
     )
     
