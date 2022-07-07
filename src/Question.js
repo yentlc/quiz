d6bba5dd6a0b492aca1d0760eba5bed8e1853936
yentl <yentlcast@gmail.com>
2022-07-07 11:49:36 -0300

    adding input type radio

--- src/Question.js
+++ src/Question.js
@@ -1,11 +1,18 @@
+import { nanoid } from 'nanoid'
 
 
 export default function Question(props){
 
+
     return(
         <fieldset>
             <legend>{props.question}</legend>
-            {props.answers.sort().map(ans => <p>{ans}</p>)}
+            {props.answers.sort().map(ans => 
+                    <div>
+                    <input key={nanoid()}type="radio" id={ans} name={props.question} value={ans} isChecked={"ver que poner"} onClick={()=>console.log("Question is clicked ->", ans)}/>
+                    <label for={ans}>{ans}</label>
+                    </div>
+                    )}
         </fieldset>
     )
     
